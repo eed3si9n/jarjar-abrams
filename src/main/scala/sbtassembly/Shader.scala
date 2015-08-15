794ecacb5119b9be3c203de7f251f50824ed3c5f
Wu Xiang <w.xiang7@gmail.com>
2015-08-15 21:36:15 +0800

    Added shading rules backed by jarjar

--- src/main/scala/sbtassembly/Shader.scala
+++ src/main/scala/sbtassembly/Shader.scala
@@ -2,79 +2,39 @@ package sbtassembly
 
 import java.io.File
 
-import org.pantsbuild.jarjar.{JJProcessor, _}
-import org.pantsbuild.jarjar.util.EntryStruct
+import org.pantsbuild.jarjar.ext_util.EntryStruct
+import org.pantsbuild.jarjar.{JJProcessor, Keep, Zap, Rule}
+
 import sbt._
 
-case class ShadeRule(shadePattern: ShadePattern, targets: Seq[ShadeTarget] = Seq()) {
-  def inLibrary(moduleId: ModuleID*): ShadeRule =
-    this.copy(targets = targets ++ (moduleId.toSeq map { x => ShadeTarget(moduleID = Some(x)) }))
-  def inAll: ShadeRule =
-    this.copy(targets = targets :+ ShadeTarget(inAll = true))
-  def inProject: ShadeRule =
-    this.copy(targets = targets :+ ShadeTarget(inProject = true))
+object Shader {
 
-  private[sbtassembly] def isApplicableTo(mod: ModuleID): Boolean =
-    targets.exists(_.isApplicableTo(mod))
+  def rename(patterns: (String, String)*): ShadeRule =
+    ShadeRule(rule = "rename", renames = patterns.toMap)
 
-  private[sbtassembly] def isApplicableToAll: Boolean =
-    targets.exists(_.inAll)
+  def remove(patterns: String*): ShadeRule =
+    ShadeRule(rule = "remove", patterns = patterns.toSet)
 
-  private[sbtassembly] def isApplicableToCompiling: Boolean =
-    targets.exists(_.inAll) || targets.exists(_.inProject)
+  def keepOnly(patterns: String*): ShadeRule =
+    ShadeRule(rule = "keepOnly", patterns = patterns.toSet)
 
-}
-
-sealed trait ShadePattern {
-  def inLibrary(moduleId: ModuleID*): ShadeRule =
-    ShadeRule(this, moduleId.toSeq map { x => ShadeTarget(moduleID = Some(x)) })
-  def inAll: ShadeRule =
-    ShadeRule(this, Seq(ShadeTarget(inAll = true)))
-  def inProject: ShadeRule =
-    ShadeRule(this, Seq(ShadeTarget(inProject = true)))
-}
-
-object ShadeRule {
-  def rename(patterns: (String, String)*): ShadePattern = Rename(patterns.toSeq.toList)
-  def zap(patterns: String*): ShadePattern = Zap(patterns.toSeq.toList)
-  def keep(patterns: String*): ShadePattern = Keep(patterns.toSeq.toList)
-  private[sbtassembly] case class Rename(patterns: List[(String, String)]) extends ShadePattern
-  private[sbtassembly] case class Zap(patterns: List[String]) extends ShadePattern
-  private[sbtassembly] case class Keep(patterns: List[String]) extends ShadePattern
-}
-
-private[sbtassembly] case class ShadeTarget(
-  inAll: Boolean = false,
-  inProject: Boolean = false,
-  moduleID: Option[ModuleID] = None) {
-
-  private[sbtassembly] def isApplicableTo(mod: ModuleID): Boolean =
-    inAll || (moduleID match {
-      case Some(m) if (m.organization == mod.organization) && (m.name == mod.name) && (m.revision == mod.revision) =>
-        true
-      case _ =>
-        false
-    })
-}
-
-private[sbtassembly] object Shader {
-  def shadeDirectory(rules: Seq[ShadeRule], dir: File, log: Logger, level: Level.Value): Unit = {
-    val jjrules = rules flatMap { r => r.shadePattern match {
-      case ShadeRule.Rename(patterns) =>
-        patterns.map { case (from, to) =>
+  private[sbtassembly] def shadeDirectory(rules: Seq[ShadeRule], dir: File, log: Logger): Unit = {
+    val jjrules = rules flatMap { r => r.rule match {
+      case "rename" =>
+        r.renames.map { case (from, to) =>
           val jrule = new Rule()
           jrule.setPattern(from)
           jrule.setResult(to)
           jrule
         }
-      case ShadeRule.Zap(patterns) =>
-        patterns.map { case pattern =>
+      case "remove" =>
+        r.patterns.map { case pattern =>
           val jrule = new Zap()
           jrule.setPattern(pattern)
           jrule
         }
-      case ShadeRule.Keep(patterns) =>
-        patterns.map { case pattern =>
+      case "keepOnly" =>
+        r.patterns.map { case pattern =>
           val jrule = new Keep()
           jrule.setPattern(pattern)
           jrule
@@ -82,28 +42,52 @@ private[sbtassembly] object Shader {
       case _ => Nil
     }}
 
-    val proc = new JJProcessor(jjrules, verbose = level == Level.Debug, true, null)
-
-    /*
-    jarjar MisplacedClassProcessor class transforms byte[] to a class using org.objectweb.asm.ClassReader.getClassName
-    which always translates class names containing '.' into '/', regardless of OS platform.
-    We need to transform any windows file paths in order for jarjar to match them properly and not omit them.
-     */
-    val files = AssemblyUtils.getMappings(dir, Set()).map(f =>
-      if (f._2.contains('\\')) (f._1, f._2.replace('\\', '/')) else f)
+    val proc = JJProcessor(jjrules, true, true)
+    val files = AssemblyUtils.getMappings(dir, Set())
 
     val entry = new EntryStruct
     files filter (!_._1.isDirectory) foreach { f =>
       entry.data = IO.readBytes(f._1)
       entry.name = f._2
       entry.time = -1
-      entry.skipTransform = false
-      IO.delete(f._1)
-      if (proc.process(entry)) {
-        IO.write(dir / entry.name, entry.data)
-      }
+
+      proc.process(entry)
+
+      IO.write(dir / entry.name, entry.data)
+      if (f._2 != entry.name) IO.delete(f._1)
     }
-    val excludes = proc.getExcludes
-    excludes.foreach(exclude => IO.delete(dir / exclude))
   }
+
+}
+
+case class ShadeTarget(toCompiling: Boolean = false,
+                       group: Option[String] = None,
+                       artifact: Option[String] = None,
+                       version: Option[String] = None) {
+  private[sbtassembly] def isApplicableTo(mod: ModuleID): Boolean =
+    group.isDefined && group.get == mod.organization &&
+      artifact.isDefined && artifact.get == mod.name &&
+      (version.isEmpty || version.get == mod.revision)
+}
+
+case class ShadeRule(rule: String,
+                     renames: Map[String, String] = Map(),
+                     patterns: Set[String] = Set(),
+                     targets: Seq[ShadeTarget] = Seq()) {
+
+  def applyToCompiling: ShadeRule =
+    this.copy(targets = targets :+ ShadeTarget(true))
+
+  def applyTo(group: String, artifact: String): ShadeRule =
+    this.copy(targets = targets :+ ShadeTarget(group = Some(group), artifact = Some(artifact)))
+
+  def applyTo(group: String, artifact: String, version: String): ShadeRule =
+    this.copy(targets = targets :+ ShadeTarget(group = Some(group), artifact = Some(artifact), version = Some(version)))
+
+  private[sbtassembly] def isApplicableTo(mod: ModuleID): Boolean =
+    targets.exists(_.isApplicableTo(mod))
+
+  private[sbtassembly] def isApplicableToCompiling: Boolean =
+    targets.exists(_.toCompiling)
+
 }
