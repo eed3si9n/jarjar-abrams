3f5d64ba87fdfff3bacdbf26589a08477265902b
Jeroen ter Voorde <jeroen.ter.voorde@simacan.com>
2020-04-07 13:57:15 +0200

    Refactored TaggedEntries and simplified the rename loop.

--- src/main/scala/org/pantsbuild/jarjar/TaggedEntry.scala
+++ src/main/scala/org/pantsbuild/jarjar/TaggedEntry.scala
@@ -8,24 +8,25 @@ trait TaggedEntry {
   def tag: Int
   def toBytes: Array[Byte]  // Entry payload. Does not include the tag or length.
 }
+
+/**
+ * Unparsed entry
+ */
 case class RawEntry(tag: Int, bytes: Array[Byte]) extends TaggedEntry {
   override def toBytes: Array[Byte] = bytes
 }
 
-abstract class Name(val tag: Int) extends TaggedEntry {
-  val name: String
-
+/**
+ * Named entry. Either a term name or type name
+ */
+case class NameEntry(tag: Int, name: String) extends TaggedEntry {
   override def toBytes: Array[Byte] = name.getBytes("UTF-8")
 }
 
-case class TermName(name: String) extends Name(PickleFormat.TERMname)
-case class TypeName(name: String) extends Name(PickleFormat.TYPEname)
-
-abstract class Ref(val tag: Int) extends TaggedEntry {
-  val nameRef: Int
-  val ownerRef: Option[Int]
-
-  def update(newNameRef: Int, newOwnerRef: Option[Int]): Ref
+/**
+ * Ref entry. Either a EXTMODCLASSref or EXTref
+ */
+case class RefEntry(tag: Int, nameRef: Int, ownerRef: Option[Int]) extends TaggedEntry {
 
   override def toBytes: Array[Byte] = {
     val os = new ByteArrayOutputStream()
@@ -45,30 +46,17 @@ abstract class Ref(val tag: Int) extends TaggedEntry {
   }
 }
 
-case class ExtModClassRef(nameRef: Int, ownerRef: Option[Int]) extends Ref(PickleFormat.EXTMODCLASSref) {
-  def update(newNameRef: Int, newOwnerRef: Option[Int]): Ref = ExtModClassRef(newNameRef, newOwnerRef)
-}
-case class ExtRef(nameRef: Int, ownerRef: Option[Int]) extends Ref(PickleFormat.EXTref) {
-  def update(newNameRef: Int, newOwnerRef: Option[Int]): Ref = ExtRef(newNameRef, newOwnerRef)
-}
 
 object TaggedEntry {
   def apply(tag: Int, bytes: Array[Byte]): TaggedEntry = {
     tag match {
-      case PickleFormat.TERMname =>
-        TermName(new String(bytes, "UTF-8"))
-      case PickleFormat.TYPEname =>
-        TypeName(new String(bytes, "UTF-8"))
-      case PickleFormat.EXTMODCLASSref =>
+      case tag @ (PickleFormat.TERMname | PickleFormat.TYPEname)=>
+        NameEntry(tag, new String(bytes, "UTF-8"))
+      case tag @ (PickleFormat.EXTMODCLASSref | PickleFormat.EXTref) =>
         val r = new ByteArrayReader(bytes)
         val nameRef = r.readNat()
         val symbolRef = if (r.atEnd) None else Some(r.readNat())
-        ExtModClassRef(nameRef, symbolRef)
-      case PickleFormat.EXTref =>
-        val r = new ByteArrayReader(bytes)
-        val nameRef = r.readNat()
-        val symbolRef = if (r.atEnd) None else Some(r.readNat())
-        ExtRef(nameRef, symbolRef)
+        RefEntry(tag, nameRef, symbolRef)
       case _ =>
         RawEntry(tag, bytes)
     }
