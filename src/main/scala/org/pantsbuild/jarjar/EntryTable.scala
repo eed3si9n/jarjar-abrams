58ee986963cf8b9cea8a67b6a98633a0b7924cff
Jeroen ter Voorde <jeroen.ter.voorde@simacan.com>
2020-04-07 13:57:15 +0200

    Refactored TaggedEntries and simplified the rename loop.

--- src/main/scala/org/pantsbuild/jarjar/EntryTable.scala
+++ src/main/scala/org/pantsbuild/jarjar/EntryTable.scala
@@ -7,15 +7,10 @@ import scala.collection.mutable
 import scala.reflect.internal.pickling.PickleFormat
 
 class EntryTable(val majorVersion: Int, minorVersion: Int, var entries: mutable.Buffer[TaggedEntry]) {
-  private val termIndices: mutable.Map[String, Int] = mutable.HashMap(
+  // Mapping of known TermName or TypeNames to their index in the table.
+  private val nameIndices: mutable.Map[NameEntry, Int] = mutable.HashMap(
     entries.zipWithIndex.collect {
-      case (entry: TermName, index) => (entry.name, index)
-    }:_*
-  )
-
-  private val typeIndices: mutable.Map[String, Int] = mutable.HashMap(
-    entries.zipWithIndex.collect {
-      case (entry: TypeName, index) => (entry.name, index)
+      case (entry: NameEntry, index) => (entry, index)
     }:_*
   )
 
@@ -31,70 +26,50 @@ class EntryTable(val majorVersion: Int, minorVersion: Int, var entries: mutable.
       result
     }
 
-    entries.zipWithIndex.foreach {
-      case (ref: Ref, index) =>
+    entries.zipWithIndex.collect {
+      case (ref: RefEntry, index) =>
         entries(ref.nameRef) match {
-          case _: TermName =>
+          case nameEntry: NameEntry =>
             resolveRef(ref).foreach { name =>
               replaceHelper(name).foreach { replaced =>
+
                 val parts = replaced.split('/')
 
                 val myOwner = parts.init.foldLeft(Option.empty[Int]) { (owner, part) =>
-                  val nextOwner = appendEntry(ExtModClassRef(getOrInsertTermName(part), owner))
+                  val nameIndex = getOrInsertTermName(NameEntry(PickleFormat.TERMname, part))
+                  val nextOwner = appendEntry(RefEntry(PickleFormat.EXTMODCLASSref, nameIndex, owner))
                   Some(nextOwner)
                 }
 
-                entries(index) = ref.update(getOrInsertTermName(parts.last), myOwner)
+                entries(index) = ref.copy(nameRef = getOrInsertTermName(nameEntry.copy(name = parts.last)), ownerRef = myOwner)
               }
             }
-
-          case _: TypeName =>
-            resolveRef(ref).foreach { name =>
-              replaceHelper(name).foreach { replaced =>
-                val parts = replaced.split('/')
-
-                val myOwner = parts.init.foldLeft(Option.empty[Int]) { (owner, part) =>
-                  val nextOwner = appendEntry(ExtModClassRef(getOrInsertTermName(part), owner))
-                  Some(nextOwner)
-                }
-
-                entries(index) = ref.update(getOrInsertTypeName(parts.last), myOwner)
-              }
-            }
+          case other =>
+            throw new RuntimeException(s"Ref entry does not point to a name but to a ${other.tag}")
         }
-
-
-      case _ => // Ignore
     }
   }
 
-  private def getOrInsertTermName(name: String): Int = {
-    termIndices.getOrElse(name, appendEntry(TermName(name)))
-  }
-
-  private def getOrInsertTypeName(name: String): Int = {
-    typeIndices.getOrElse(name, appendEntry(TypeName(name)))
+  private def getOrInsertTermName(name: NameEntry): Int = {
+    nameIndices.getOrElse(name, appendEntry(name))
   }
 
   private def appendEntry(name: TaggedEntry): Int = {
     val index = entries.size
     entries += name
     name match {
-      case termName: TermName =>
-        termIndices.put(termName.name, index)
-      case typeName: TypeName =>
-        typeIndices.put(typeName.name, index)
-
+      case name: NameEntry =>
+        nameIndices.put(name, index)
       case _ => // NoOp
     }
 
     index
   }
 
-  def resolveRef(extMod: Ref): Option[String] = {
+  def resolveRef(extMod: RefEntry): Option[String] = {
 
     val myName = entries(extMod.nameRef) match {
-      case term: Name => term.name
+      case term: NameEntry => term.name
       case raw: RawEntry => throw new RuntimeException(s"Unexpected raw type for nameref ${raw.tag}")
       case other => throw new RuntimeException(s"Unexpected type for nameref $other")
     }
@@ -102,15 +77,14 @@ class EntryTable(val majorVersion: Int, minorVersion: Int, var entries: mutable.
       case None => Some(myName)
       case Some(owner) =>
         entries(owner) match {
-          case name: TermName =>
+          case name: NameEntry =>
             Some(s"$name/$myName")
-          case mod: Ref =>
+          case mod: RefEntry =>
             resolveRef(mod).map(p => s"$p/$myName")
           case raw: RawEntry if raw.tag == PickleFormat.NONEsym =>
             None
           case raw: RawEntry =>
-            println(s"Not a known owner type tag for $myName : ${raw.tag}")
-            None
+            throw new RuntimeException(s"Not a known owner type tag for $myName : ${raw.tag}")
         }
     }
   }
