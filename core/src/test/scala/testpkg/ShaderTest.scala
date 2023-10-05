62a8f69cb5f2cc818dec7af6a8ec8b1e12800faa
Eugene Yokota <eed3si9n@gmail.com>
2023-10-05 11:32:19 -0400

    Try to reproduce shapeless issue

--- core/src/test/scala/testpkg/ShaderTest.scala
+++ core/src/test/scala/testpkg/ShaderTest.scala
@@ -1,18 +1,25 @@
 package testpkg
 
 import verify._
-import java.nio.file.{ Files, Paths }
+import java.nio.file.{ Files, Path, Paths }
 import com.eed3si9n.jarjarabrams.{ Main, Shader, Zip }
 
 object ShaderTest extends BasicTestSuite {
-  test("shade a file") {
+  test("shade bytebuddy") {
+    testShading(Paths.get("example/byte-buddy-agent.jar"), "foo/Attacher.class")
+  }
+
+  test("shade shapeless") {
+    testShading(Paths.get("example/shapeless_2.12-2.3.2.jar"), "bar/shapeless/Poly8.class")
+  }
+
+  def testShading(inJar: Path, expectedClass: String): Unit = {
     val tempJar = Files.createTempFile("test", ".jar")
     val rules = Paths.get("example/shade.rules")
-    val inJar = Paths.get("example/byte-buddy-agent.jar")
     new Main().process(rules, inJar, tempJar)
     val tempDir = Files.createTempDirectory("jarjartest")
     Zip.unzip(tempJar, tempDir)
     val entries = Shader.makeMappings(tempDir).map(_._2)
-    assert(entries.contains("foo/Attacher.class"))
+    assert(entries.contains(expectedClass))
   }
 }
