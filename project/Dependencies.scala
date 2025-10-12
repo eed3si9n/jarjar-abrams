ccecdd59be27a16b28423ec9974d11f1e72c9d67
xuwei-k <6b656e6a69@gmail.com>
2025-10-12 13:19:27 +0900

    remove unnecessary build settings

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,6 @@
 import sbt._
 
 object Dependencies {
-  val scala210 = "2.10.7"
-  val scala211 = "2.11.12"
   val scala212 = "2.12.20"
   val scala213 = "2.13.16"
   val verify = "com.eed3si9n.verify" %% "verify" % "0.2.0"
