5615714566c08e1d9f7d8a29d0d02ee5441c6265
kenji yoshida <6b656e6a69@gmail.com>
2025-10-13 07:28:38 +0900

    add integrationTest project. fix warnings (#75)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,4 +7,8 @@ object Dependencies {
   val scala213 = "2.13.16"
   val verify = "com.eed3si9n.verify" %% "verify" % "0.2.0"
   val parallel = "org.scala-lang.modules" %% "scala-parallel-collections" % "1.0.2"
+  val junit = Seq(
+    "junit" % "junit" % "4.12" % "test",
+    "com.github.sbt" % "junit-interface" % "0.13.2" % "test"
+  )
 }
